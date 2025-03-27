fa7454dbbb143a65d11e70f3203d890f639191b5
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2025-03-27 14:19:26 +0800

    update

---
+++ LS10/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace LS10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // while 语句
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            // break 跳出循环
+            while (true)
+            {
+                Console.WriteLine("请输入一个数字：");
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    break;
+                }
+            }
+
+            // continue 跳过本次循环
+            int a = 0;
+            while (a <= 20)
+            {
+                if (a % 2 == 0)
+                {
+                    a++;
+                    continue;
+                }
+                Console.WriteLine(a);
+                a++;
+            }
+
+            // do while 语句
+            // 先执行一次循环体，再判断条件
+            int b = 0;
+            do
+            {
+                Console.WriteLine(b);
+                b++;
+            }while (b <= 10);
+
+            // for 语句
+            for (int c = 0; c < 10; c++)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
--- LS9/Program.cs
+++ LS9/Program.cs
@@ -5,13 +5,55 @@
         static void Main(string[] args)
         {
             // if条件语句
-            // if (条件)
+            // if (条件表达式或bool类型)
             // {
             //     // 条件为真时执行的代码
             // }
             if (true)
             {
                 Console.WriteLine("条件为真");
+            }
+
+            // if else条件语句
+            int a = 1;
+            if (a > 5)
+            {
+                Console.WriteLine("a大于5");
+            }
+            else
+            {
+                Console.WriteLine("a小于等于5");
+            }
+
+            // if...else if...else条件语句
+            if (a > 5)
+            {
+                Console.WriteLine("a大于5");
+            }
+            else if (a == 5)
+            {
+                Console.WriteLine("a等于5");
+            }
+            else
+            {
+                Console.WriteLine("a小于5");
+            }
+
+            // switch条件语句
+            // switch 只能判断固定的值
+            // 和 C++ 一样，这里的switch语句也是要写break的，不然就会贯穿
+            a = 100;
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("a等于1");
+                    break;
+                case 2:
+                    Console.WriteLine("a等于2");
+                    break;
+                default: // default可以省略
+                    Console.WriteLine("a不等于1也不等于2");
+                    break;
             }
         }
     }
