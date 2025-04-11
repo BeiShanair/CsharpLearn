c56b0b7bc02fd5929f86d303107587e4aeb9f070
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2025-04-11 19:53:38 +0800

    update

---
+++ S3_5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace S3_5
+{
+    // 密封类
+    // 让类不能被继承
+    // 语法：sealed class 类名
+    sealed class Father
+    {
+
+    }
+    // class Son : Father // 会报错
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ S3_6/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace S3_6
+{
+    // 多态
+    // 概念：同一个方法，不同的对象调用，表现出不同的行为
+    // 虚函数、重写、父类
+    // virtual、override、base
+    class GameObject
+    {
+        public string name;
+        public GameObject(string name)
+        {
+            this.name = name;
+        }
+        // 虚函数用于提供子类重写
+        public virtual void Atk()
+        {
+            Console.WriteLine("普通攻击");
+        }
+    }
+    class Player : GameObject
+    {
+        public Player(string name) : base(name)
+        {
+
+        }
+
+        public override void Atk()
+        {
+            // 可以通过base保留父类的逻辑（类似super）
+            base.Atk();
+            Console.WriteLine("玩家攻击");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameObject obj = new Player("玩家");
+            obj.Atk();
+        }
+    }
+}
